ca1cfa83afe8ccd50a25e240d60f064bfcec42f6
hansonfuel <hansonfuel01@gmail.com>
2025-04-10 02:45:20 +0000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Add this to all pages
-supabase.auth.onAuthStateChange((event, session) => {
-	if (window.location.pathname.includes('admin.html')) {
-	  if (!session?.user) window.location.href = 'index.html';
-	}
-  });
